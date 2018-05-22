2372bb89bd077786a2410bb92eb999723ef0d54d
danielconner <metalman1342@gmail.com>
2018-05-22 10:41:02 -0500

    fixed primary key

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,11 +3,12 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
-  id INT UNSIGNED AUTO_INCREMENT,
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   artist VARCHAR(100),
   name VARCHAR(100),
   release_date INT(4),
   sales FLOAT,
-  genre VARCHAR(100)
+  genre VARCHAR(100),
+  PRIMARY KEY (id)
 );
 
