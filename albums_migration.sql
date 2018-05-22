0365e2cd924bd50279eefabbe3c7474d9bd89c79
danielconner <metalman1342@gmail.com>
2018-05-22 10:33:51 -0500

    created

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+  id INT UNSIGNED AUTO_INCREMENT,
+  artist VARCHAR(100),
+  name VARCHAR(100),
+  release_date INT(4),
+  sales FLOAT,
+  genre VARCHAR(100)
+);
+
