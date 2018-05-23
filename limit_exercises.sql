525aec32cd2c6ba2d197eac97af8484515b9ce93
danielconner <metalman1342@gmail.com>
2018-05-23 16:53:39 -0500

    pt.3

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -6,4 +6,4 @@ LIMIT 10;
 
 SELECT first_name, last_name, birth_date, hire_date FROM employees WHERE birth_date LIKE '%-12-25' AND hire_date LIKE '199%'
 ORDER BY birth_date, hire_date DESC
-LIMIT 5;
+LIMIT 5 OFFSET 45;
