567ac4b7638abfb2dc6a288be5ca8f5fdace4035
danielconner <metalman1342@gmail.com>
2018-05-23 15:58:18 -0500

    part 4

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -3,9 +3,11 @@ USE employees;
 SELECT first_name, last_name FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M'
 ORDER BY last_name, first_name;
 
-SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e'
+ORDER BY emp_no;
 
-SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e'
+ORDER BY emp_no;
 
 -- SELECT first_name, last_name, hire_date FROM employees WHERE hire_date LIKE '199%';
 
