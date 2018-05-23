ca71755c8758b4b3f32a777132cf6bfbabd08b4b
danielconner <metalman1342@gmail.com>
2018-05-23 15:21:50 -0500

    part two complete

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,12 +1,14 @@
 USE employees;
 
-SELECT first_name, last_name FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+SELECT first_name, last_name FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+                                                  AND gender = 'M';
 
-SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%';
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
 
-SELECT first_name, last_name, hire_date FROM employees WHERE hire_date LIKE '199%';
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
 
-SELECT first_name, last_name, birth_date FROM employees WHERE birth_date LIKE '%-12-25';
+-- SELECT first_name, last_name, hire_date FROM employees WHERE hire_date LIKE '199%';
 
-SELECT first_name, last_name FROM employees WHERE last_name LIKE '%q%';
+SELECT first_name, last_name, birth_date, hire_date FROM employees WHERE birth_date LIKE '%-12-25' AND hire_date LIKE '199%';
 
+SELECT first_name, last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
