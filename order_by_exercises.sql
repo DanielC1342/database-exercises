fb6e41fd6a742ee914548033a0ca8bcff28aa9cd
danielconner <metalman1342@gmail.com>
2018-05-23 15:57:19 -0500

    part 3

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,7 +1,7 @@
 USE employees;
 
 SELECT first_name, last_name FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M'
-ORDER BY first_name, last_name;
+ORDER BY last_name, first_name;
 
 SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
 
