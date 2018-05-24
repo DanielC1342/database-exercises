d33107db1425b48bb29e7ff31618e0d0b029bf10
danielconner <metalman1342@gmail.com>
2018-05-24 09:18:58 -0500

    pt.1

---
+++ function_exercises.sql
@@ -0,0 +1,17 @@
+USE employees;
+
+SELECT first_name, last_name FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M'
+ORDER BY last_name, first_name;
+
+SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e'
+ORDER BY emp_no DESC;
+
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e'
+ORDER BY emp_no DESC;
+
+-- SELECT first_name, last_name, hire_date FROM employees WHERE hire_date LIKE '199%';
+
+SELECT first_name, last_name, birth_date, hire_date FROM employees WHERE birth_date LIKE '%-12-25' AND hire_date LIKE '199%'
+ORDER BY birth_date, hire_date DESC;
+
+SELECT first_name, last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
