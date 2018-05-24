8508822333de8036fe02bc924c4add50d88c4b80
danielconner <metalman1342@gmail.com>
2018-05-24 11:06:49 -0500

    pt.2

--- function_exercises.sql
+++ function_exercises.sql
@@ -3,7 +3,7 @@ USE employees;
 SELECT first_name, last_name FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M'
 ORDER BY last_name, first_name;
 
-SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e'
+SELECT CONCAT(first_name, ' ', last_name) AS 'full_name' FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e'
 ORDER BY emp_no DESC;
 
 SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e'
@@ -11,7 +11,7 @@ ORDER BY emp_no DESC;
 
 -- SELECT first_name, last_name, hire_date FROM employees WHERE hire_date LIKE '199%';
 
-SELECT first_name, last_name, birth_date, hire_date FROM employees WHERE birth_date LIKE '%-12-25' AND hire_date LIKE '199%'
+SELECT CONCAT(first_name, ' ', last_name) AS 'emp_name', DATEDIFF(CURDATE(),hire_date) AS 'days_employed', ROUND((DATEDIFF(CURDATE(),hire_date))/365) AS 'years_employed' FROM employees WHERE birth_date LIKE '%-12-25' AND hire_date LIKE '199%'
 ORDER BY birth_date, hire_date DESC;
 
 SELECT first_name, last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
