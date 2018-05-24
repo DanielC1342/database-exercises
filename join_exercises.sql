4929863fb341e5bba418a1f9c732c36c46058051
danielconner <metalman1342@gmail.com>
2018-05-24 16:53:52 -0500

    finished pt.2

--- join_exercises.sql
+++ join_exercises.sql
@@ -4,5 +4,5 @@ SELECT de.dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name) AS 
 FROM employees AS e
 JOIN dept_manager AS dm ON e.emp_no = dm.emp_no
 JOIN departments AS de ON dm.dept_no = de.dept_no
-WHERE dm.to_date = '9999-01-01'
+WHERE dm.to_date = '9999-01-01' AND e.gender = 'F'
 GROUP BY `Department Name`, e.first_name, e.last_name;
