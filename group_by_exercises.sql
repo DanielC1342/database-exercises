b14784fe3f889259c148af0d5655b9b62bb37d36
danielconner <metalman1342@gmail.com>
2018-05-24 12:00:03 -0500

    complete

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -2,5 +2,11 @@ USE employees;
 
 SELECT DISTINCT title FROM titles;
 
-SELECT DISTINCT last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e'
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e'
+GROUP BY last_name, first_name;
+
+SELECT last_name, COUNT(last_name) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
 GROUP BY last_name;
+
+SELECT CONCAT(COUNT(*), ' ', gender) FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+GROUP BY gender;
