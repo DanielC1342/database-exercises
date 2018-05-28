c7be94111d31f85fb3a7bfa58571fd4cf30b6b16
danielconner <metalman1342@gmail.com>
2018-05-27 19:26:54 -0500

    finished the rest

--- join_exercises.sql
+++ join_exercises.sql
@@ -6,3 +6,19 @@ JOIN dept_manager AS dm ON e.emp_no = dm.emp_no
 JOIN departments AS de ON dm.dept_no = de.dept_no
 WHERE dm.to_date = '9999-01-01' AND e.gender = 'F'
 GROUP BY `Department Name`, e.first_name, e.last_name;
+
+SELECT t.title AS 'Title', COUNT(de.emp_no) AS 'Count'
+FROM titles AS t
+JOIN employees AS e ON t.emp_no = e.emp_no
+JOIN dept_emp AS de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Customer Service' AND t.to_date = '9999-01-01'
+GROUP BY Title;
+
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Name', s.salary AS 'Salary'
+FROM departments AS d
+JOIN dept_manager AS dm ON d.dept_no = dm.dept_no
+JOIN salaries AS s ON dm.emp_no = s.emp_no
+JOIN employees e ON dm.emp_no = e.emp_no
+WHERE s.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
+GROUP BY `Department Name`, e.first_name, e.last_name, s.salary;
