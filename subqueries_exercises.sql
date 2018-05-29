edf1d20d67149e870e12d5ef2d25e6c904e4d3d1
danielconner <metalman1342@gmail.com>
2018-05-29 09:39:36 -0500

    pt2 and pt3 done

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -7,3 +7,17 @@ WHERE hire_date = (
   FROM employees
   WHERE emp_no = 101010
 );
+
+SELECT t.title, CONCAT(e.first_name, ' ', e.last_name) AS 'Name'
+FROM titles AS t
+JOIN employees AS e ON t.emp_no = e.emp_no
+WHERE e.first_name = 'Aamod'
+ORDER BY title;
+
+SELECT first_name, last_name
+FROM employees AS e
+JOIN dept_manager AS m ON e.emp_no = m.emp_no
+WHERE gender = 'F' AND to_date = '9999-01-01' AND e.emp_no IN (
+  SELECT m.emp_no
+  FROM dept_manager
+);
